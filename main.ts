9213b4c28796fae26bcc7c7a2913f4f34c54e10e
jcarpon016 <132546629+jcarpon016@users.noreply.github.com>
2023-05-17 20:34:19 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let fijar_d = 0
+basic.forever(function () {
+    let d = 0
+    fijar_d = input.compassHeading()
+    if (Math.abs(d - 0) <= 22) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
